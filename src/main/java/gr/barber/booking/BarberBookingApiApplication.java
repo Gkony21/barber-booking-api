557c5fcc234733cf2372a11f1a848d6f31a88bf1
Giorgos Koniditsiotis <george_kony@hotmail.com>
2026-07-15 17:07:22 +0300

    chore: initial project setup

---
+++ src/main/java/gr/barber/booking/BarberBookingApiApplication.java
@@ -0,0 +1,13 @@
+package gr.barber.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberBookingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarberBookingApiApplication.class, args);
+    }
+
+}
