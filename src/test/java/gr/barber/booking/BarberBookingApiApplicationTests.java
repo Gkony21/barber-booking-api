d3c022f2e55925dbafa51412510f4b33c54e8662
Giorgos Koniditsiotis <george_kony@hotmail.com>
2026-07-15 17:07:22 +0300

    chore: initial project setup

---
+++ src/test/java/gr/barber/booking/BarberBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.barber.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberBookingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
