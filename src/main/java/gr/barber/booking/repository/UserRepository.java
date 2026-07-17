972a1d6788b1a9cbf2aba886ac066162e5d5bd96
Giorgos Koniditsiotis <george_kony@hotmail.com>
2026-07-17 21:39:59 +0300

    feat: add user repository

---
+++ src/main/java/gr/barber/booking/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package gr.barber.booking.repository;
+
+import gr.barber.booking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
