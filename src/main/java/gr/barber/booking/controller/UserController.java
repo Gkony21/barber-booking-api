3e1ebf510496649c87d2d725d8f81ce2099dea02
Giorgos Koniditsiotis <george_kony@hotmail.com>
2026-07-20 02:25:17 +0300

    feat: create user endpoint

--- src/main/java/gr/barber/booking/controller/UserController.java
+++ src/main/java/gr/barber/booking/controller/UserController.java
@@ -1,9 +1,11 @@
 package gr.barber.booking.controller;
 
+import gr.barber.booking.model.User;
 import gr.barber.booking.service.UserService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -11,4 +13,10 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
 }
